bf5a801ee70f2929be29317524610735417207bb
yassine-abdlemalak <yassine.abdlemalak@edu.isetcom.tn>
2026-09-15 16:54:54 +0100

    TP1: Initial commit TestEnv.java

---
+++ public class TestEnv {.java
@@ -0,0 +1,7 @@
+class TestEnv {
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println(" ENVIRONNEMENT VISUAL STUDIO + JAVA PRÊT !");
+        System.out.println("================================");
+    }
+}
